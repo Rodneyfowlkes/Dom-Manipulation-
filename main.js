f3356941dfb0c7ccda24b931ec5f863d8d621b74
Rodney Fowlkes <Fowlkesrodney@gmail.com>
2016-06-14 18:08:09 -0400

    Adding more functions to the calculator

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
+// Variables
+
 var first_num = document.querySelector("#value-one");
 
 var second_num = document.querySelector("#value-two");
 
-var add_button = document.querySelector("#calculate-button");
+var add_button = document.querySelector("#add-button");
 
+var subtract_button = document.querySelector("#subtract-button");
+
+var multiply_button = document.querySelector("#multiply-button");
+
+var divide_button = document.querySelector("#divide-button");
+
+var square_button = document.querySelector("#square-button");
+
+
+// Add Function
 
 var add = function ( x, y) {
 
@@ -12,6 +24,63 @@ var sum = Number(x) + Number(y)
 return sum;
 
 };
+
+// Subtract Function
+
+var subtract = function ( x, y) {
+
+var difference = Number(x) - Number(y) 
+
+return difference;
+
+};
+
+
+
+
+// Multiply Function
+
+var multiply = function ( x, y) {
+
+var multiple = Number(x) * Number(y) 
+
+return multiple;
+
+};
+
+// Divide Function
+
+var divide = function ( x, y) {
+
+var quotient = Number(x) / Number(y) 
+
+return quotient;
+
+};
+
+// Square
+
+
+var square = function (x) {
+
+
+var squareroot =  Math.sqrt(Number(x))
+
+return squareroot;
+
+};
+
+
+
+
+
+
+
+
+
+
+// Add button
+
 
 add_button.addEventListener('click', function(event)
 
@@ -26,6 +95,75 @@ add_button.addEventListener('click', function(event)
 
 
 });
+
+// multiply button
+
+
+multiply_button.addEventListener('click', function(event)
+
+{
+	event.preventDefault();
+
+    var answer = document.querySelector("#answer");
+    var first_num1 = first_num.value;
+    var second_num2 = second_num.value;
+    answer.textContent = multiply (first_num1, second_num2);
+
+
+
+});
+
+
+// Divide button 
+
+divide_button.addEventListener('click', function(event)
+
+{
+	event.preventDefault();
+
+    var answer = document.querySelector("#answer");
+    var first_num1 = first_num.value;
+    var second_num2 = second_num.value;
+    answer.textContent = divide (first_num1, second_num2);
+
+
+
+});
+
+
+// Subtract button
+
+subtract_button.addEventListener('click', function(event)
+
+{
+	event.preventDefault();
+
+    var answer = document.querySelector("#answer");
+    var first_num1 = first_num.value;
+    var second_num2 = second_num.value;
+    answer.textContent = subtract (first_num1, second_num2);
+
+
+
+});
+
+
+// Square button
+
+square_button.addEventListener('click', function(event)
+
+{
+	event.preventDefault();
+
+    var answer = document.querySelector("#answer");
+    var first_num1 = first_num.value;
+    var second_num2 = second_num.value;
+    answer.textContent = square (first_num1, second_num2);
+
+
+
+});
+
 
 
 
